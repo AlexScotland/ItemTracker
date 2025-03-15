96c79a6c1a198786a57bfb2be27e84684c084e4f
Alex Scotland <alex@scotland.ca>
2025-03-15 16:17:30 -0400

    :sparkles: adds 3 projects

--- itemtracker/itemtracker/settings.py
+++ itemtracker/itemtracker/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-i5-fyh)hmc7mre+!b26+!acpp0!z_e_yx5mgor!@h=!g5y$e53
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
